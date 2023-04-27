c4902906d8168d491d2fdcd9ca45f4c5ffb32901
Manuel <manuel.vanek@gmail.com>
2023-04-27 11:38:14 +0200

    added menu

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,14 +7,7 @@ public class Application {
         Account account = new Account("Manuel Vanek", "AT732060436165399561", 500.0);
         Menu menu = new Menu();
         menu.showMenu(account);
-        /*
-        Transaction transaction = account.getTransactionInformation(account);
-        transaction.processTransaction(account, transaction);
 
-        System.out.println(transaction);
-        System.out.println("balance: " + account.getBalance());
-
-         */
     }
 
 
